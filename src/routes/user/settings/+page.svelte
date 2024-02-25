443690819463c3bb000ba80f3ade99ae91391493
phartenfeller <pharti@hotmail.de>
2024-02-25 17:07:33 +0100

    gen subscription

--- src/routes/user/settings/+page.svelte
+++ src/routes/user/settings/+page.svelte
@@ -2,9 +2,18 @@
 	import { onMount } from 'svelte';
 
 	let nottifPermGranted: boolean | null = null;
+	let isSubscribed = false;
 
-	onMount(() => {
+	onMount(async () => {
 		nottifPermGranted = Notification.permission === 'granted';
+
+		if (nottifPermGranted) {
+			isSubscribed = await checkSubscriptionStatus();
+
+			if (!isSubscribed) {
+				await subscribeUser();
+			}
+		}
 	});
 
 	function requestNotificationPermission() {
@@ -13,6 +22,35 @@
 				new Notification('You are now subscribed to notifications!');
 			}
 		});
+	}
+
+	async function checkSubscriptionStatus() {
+		if ('serviceWorker' in navigator) {
+			const registration = await navigator.serviceWorker.ready;
+			const subscription = await registration.pushManager.getSubscription();
+			console.log('Subscription:', subscription);
+			return subscription !== null;
+		}
+		return false;
+	}
+
+	async function subscribeUser() {
+		if ('serviceWorker' in navigator) {
+			try {
+				const res = await fetch('/api/vapidPubKey');
+				const { data } = await res.json();
+
+				const registration = await navigator.serviceWorker.ready;
+				const subscription = await registration.pushManager.subscribe({
+					userVisibleOnly: true,
+					applicationServerKey: data
+				});
+				isSubscribed = true;
+				console.log('Subscription:', JSON.stringify(subscription));
+			} catch (err) {
+				console.error('Error subscribing:', err);
+			}
+		}
 	}
 </script>
 
@@ -34,6 +72,7 @@
 					You have enabled notification permissions. Remove the permission in your browser
 					settings...
 				</p>
+				<p>Subscribed to push notifications: <b>{isSubscribed}</b></p>
 			{/if}
 		</div>
 	</div>
